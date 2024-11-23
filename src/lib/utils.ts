2959dcde89b474f9caf284535a72af1b3d25bf68
Matthew Fournier <matthew@halfhelix.com>
2024-11-23 09:08:16 -0500

    html wait events

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { rejects } from 'assert'
 import { JSDOM } from 'jsdom'
 
 export const pause = (waitTime: number) =>
@@ -11,10 +12,39 @@ export const createDom = (htmlString: string) => {
   const body = document.body
   const Event = document.defaultView!.Event
 
+  const triggerEvent = (
+    element: HTMLElement | null,
+    eventName: string,
+    eventFunction: (event: Event) => Promise<any> | any
+  ) =>
+    new Promise((resolve, reject) => {
+      try {
+        const listenerResponse = async (event: Event) => {
+          event.preventDefault()
+          element?.removeEventListener(eventName, listenerResponse)
+          const listnerResponse = await eventFunction(event)
+          resolve(listnerResponse)
+        }
+
+        element?.addEventListener(eventName, listenerResponse)
+
+        element?.dispatchEvent(
+          new Event(eventName, {
+            bubbles: true,
+            cancelable: true
+          })
+        )
+      } catch (error) {
+        console.error(error)
+        reject(error)
+      }
+    })
+
   return {
     dom,
     document,
     body,
+    triggerEvent,
     Event
   }
 }
